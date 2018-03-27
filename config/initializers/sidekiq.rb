2f991223573fc6b09af0d93e4647af69d2db95f4
AdrielD <adriel.dinelli@gmail.com>
2018-03-27 01:25:09 -0300

    [FIX] Heroku limit

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], size: 2 }
+  config.redis = { url: ENV["REDIS_URL"], size: 5 }
 end
 
 Sidekiq.configure_client do |config|
